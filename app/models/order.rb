2e395f08131a68b3be037c2765855089edcd6834
bingo8670 <307286130@qq.com>
2017-07-16 14:39:45 +0800

    add order controller

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,8 @@
 class Order < ApplicationRecord
+  belongs_to :user
+
+  validatas :billing_name, presence: true
+  validatas :billing_address, presence: true
+  validatas :shipping_name, , presence: true
+  validatas :shipping_address, presence: true
 end
