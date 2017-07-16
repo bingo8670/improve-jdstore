e9e782d60721cf4677e8c364c39918e5d950eb72
bingo8670 <307286130@qq.com>
2017-07-16 15:09:59 +0800

    add orders show function

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,10 +1,10 @@
 class Order < ApplicationRecord
   belongs_to :user
 
-  validatas :billing_name, presence: true
-  validatas :billing_address, presence: true
-  validatas :shipping_name, , presence: true
-  validatas :shipping_address, presence: true
+  validates :billing_name, presence: true
+  validates :billing_address, presence: true
+  validates :shipping_name,  presence: true
+  validates :shipping_address, presence: true
 
   has_many :product_lists
 end
