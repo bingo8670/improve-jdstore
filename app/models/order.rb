5ad5b0e38ef9d27c1e08feaa673d6597ddc888a8
bingo8670 <307286130@qq.com>
2017-07-16 15:43:50 +0800

    add payment_method function

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,6 @@
 class Order < ApplicationRecord
   belongs_to :user
-  
+
   before_create :generate_token
 
   def generate_token
@@ -13,4 +13,12 @@ class Order < ApplicationRecord
   validates :shipping_address, presence: true
 
   has_many :product_lists
+
+  def set_payment_with!(method)
+    self.update_columns(payment_method: method)
+  end
+
+  def pay!
+    self.update_columns(is_paid: true)
+  end
 end
